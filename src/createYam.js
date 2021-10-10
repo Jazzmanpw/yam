bf3261da28b0c62055a23120c6f907bade607d65
Dmitry Shpak <hummer.revolver@gmail.com>
2021-10-10 14:58:49 +0300

    stateChangedBy

--- src/createYam.js
+++ src/createYam.js
@@ -1,9 +1,28 @@
 export default function createYam(handlers) {
   return (store) => (next) => (action) => {
+    const prevState = store.getState();
     next(action);
     const state = store.getState();
+
+    const stateChangedBy = createStateChangedBy(prevState, state);
+
     handlers.forEach((handler) =>
-      handler({ state, action, dispatch: store.dispatch }),
+      handler({
+        state,
+        action,
+        dispatch: store.dispatch,
+        stateChangedBy,
+      }),
     );
+  };
+}
+
+function createStateChangedBy(prevState, nextState) {
+  const cache = new Map();
+  return (selector) => {
+    if (!cache.has(selector)) {
+      cache.set(selector, selector(prevState) !== selector(nextState));
+    }
+    return cache.get(selector);
   };
 }
--- src/createYam.test.js
+++ src/createYam.test.js
@@ -3,6 +3,9 @@ import createYam from './createYam';
 const prevState = 'previous';
 const nextState = 'next';
 
+const changingSelector = (state) => state.toUpperCase();
+const notChangingSelector = (state) => typeof state === 'string';
+
 const store = {
   getState: jest.fn(),
   dispatch: jest.fn(),
@@ -18,31 +21,65 @@ afterEach(() => {
 
 const next = jest.fn(() => store.getState.mockReturnValue(nextState));
 
-function createDispatch(handlers) {
-  return createYam(handlers)(store)(next);
+function callYam(handlers) {
+  const action = { type: 'first' };
+  return createYam(handlers)(store)(next)(action);
 }
 
-it('calls every handler with the state, dispatch and action', () => {
+it('calls every handler with the state, dispatch, action and stateChangedBy', () => {
   const handlers = [jest.fn(), jest.fn(), jest.fn()];
   const action = { type: 'first' };
-  const dispatch = createDispatch(handlers);
 
-  dispatch(action);
+  callYam(handlers);
 
   handlers.forEach((handler) =>
     expect(handler).toBeCalledWith({
       state: nextState,
       action,
       dispatch: store.dispatch,
+      stateChangedBy: expect.any(Function),
     }),
   );
 });
 
 it.each([[[]], [[jest.fn()]], [[jest.fn(), jest.fn()]]])(
-  'calls getState once, no matter how many handlers have been passed',
+  'calls getState once before the "next" call and once after, no matter how many handlers have been passed',
   (handlers) => {
-    const dispatch = createDispatch(handlers);
-    dispatch({ type: 'first' });
-    expect(store.getState).toBeCalledTimes(1);
+    callYam(handlers);
+
+    expect(store.getState).toBeCalledTimes(2);
+    expect(store.getState.mock.results).toEqual([
+      { type: 'return', value: prevState },
+      { type: 'return', value: nextState },
+    ]);
   },
 );
+
+describe('stateChangedBy', () => {
+  it('returns true if the selector result changed', () => {
+    const handler = ({ stateChangedBy }) => {
+      expect(stateChangedBy(changingSelector)).toBe(true);
+    };
+    callYam([handler]);
+  });
+
+  it('returns false if the selector result did not change', () => {
+    const handler = ({ stateChangedBy }) => {
+      expect(stateChangedBy(notChangingSelector)).toBe(false);
+    };
+    callYam([handler]);
+  });
+
+  it('calls the selector once for previous state and one for the new one, no matter how many handlers use the selector', () => {
+    const selector = jest.fn();
+    const handlers = [
+      ({ stateChangedBy }) => stateChangedBy(selector),
+      ({ stateChangedBy }) => stateChangedBy(selector),
+    ];
+
+    callYam(handlers);
+
+    expect(selector).toBeCalledTimes(2);
+    expect(selector.mock.calls).toEqual([[prevState], [nextState]]);
+  });
+});
